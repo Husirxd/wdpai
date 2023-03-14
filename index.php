825636796cfd5bc12079d8395f8727c3db209929
Patryk <apatryk19@gmail.com>
2023-03-14 17:13:00 +0100

    basic routing

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo 'Hi there 👋';
+require 'Routing.class.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+Router::get('', 'DefaultController');
+Router::get('create-form', 'DefaultController');
+Router::post('login', 'SecurityController');
+
+Router::run($path);
