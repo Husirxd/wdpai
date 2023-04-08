ab888b5615003ff9e9144034aa200f15a0337127
Patryk <apatryk19@gmail.com>
2023-04-08 16:14:05 +0200

    create quiz frontend

--- frontend/templates/create.php
+++ frontend/templates/create.php
@@ -13,22 +13,35 @@ if(isset($messages)){
 }
 ?>
 
-    <div class="container">
+    <div class="container container--sm">
         <div class="create-container">
             <form `class="create" action="create"` method="POST" id="create-quiz" enctype="multipart/form-data">
-                <input name="title" type="text" placeholder="title">
-                <input name="category" type="text" placeholder="category">
-                <input name="is_public" type="checkbox" placeholder="is_public">
-                <input name="thumbnail" type="file" placeholder="thumbnail">
+                <div class="wide"><input name="title" type="text" placeholder="title"></div>
+                <div class="quiz-data">
+                    <div class="quiz-info">
+                        <input name="category" type="text" placeholder="category">
+                        <label class="checkbox-label"><input name="is_public" type="checkbox" placeholder="is_public">Show this quizz on the page</label>
+                    </div>
+                    <div class="quiz-thumbnail">
+                        <img src="/frontend/assets/images/placeholder.png" alt="preview" class="thumbnail-preview">
+                        <input name="thumbnail" id="thumbnail" type="file" placeholder="thumbnail">
+                    </div>
+                </div>
                 
-                //add questions
+
                 <div class="questions">
+                    <h2>Questions</h2>
                     <input type="hidden" id="question-count" name="question_count" value="1">
                     <div class="question question--template">
-                        <input class="question" name="question-0" type="text" placeholder="question">
-                        <label><input type="number" min="0" max="68" class="question__points" name="points-0"></label>
                         <input type="hidden" name="correct-0" class="question__correct" value="0">
-                        <input type="file" class="question__image" name="image_url-0">
+                        <div class="question__top">
+                            <input class="question__question" name="question-0"  type="text" placeholder="question">
+                            <input type="number" min="0" max="68" placeholder="Set Points" class="question__points" name="points-0">
+                        </div>
+                        <div class="question-thumbnail">
+                            <img src="/frontend/assets/images/placeholder.png" alt="preview" class="thumbnail-preview">
+                            <input type="file" class="question__image" name="image_url-0">
+                        </div>
                         <div class="answers">
                             <div class="answer-container">
                                 <input class="answer" name="answer-0[]" type="text" placeholder="answer">
@@ -37,13 +50,14 @@ if(isset($messages)){
                             <div class="answer-container">
                                 <input class="answer" name="answer-0[]" type="text" placeholder="answer">
                                 <button  type="button" class="set-correct">Set Correct</button>
+                                <span class="remove-answer">×</span>
                             </div>
                         </div>
                         <button type="button" class="add-answer">ADD ANSWER</button>
                     </div>
                 </div>
-                <button class="add-question" type="button">ADD QUESTION</button>
-                <button type="submit">CREATE</button>
+                <div class="flex"><button class="add-question button button--secondary" type="button">ADD QUESTION</button></div>
+                <div class="flex flex--center"><button class="button" type="submit">CREATE</button></div>
             
             
             </form>
